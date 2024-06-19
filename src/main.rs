33b07c57f578ea056c5619a68701610be24fa683
drcheeseface <tharun1@hotmail.co.uk>
2024-06-19 22:48:53 +0100

    unwrap unwrap unwrap unwrap unwrap unwrap expect

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
+use std::collections::HashMap;
 use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 
 struct Client {
     stream: TcpStream,
-    addr: String,
+    addr: SocketAddr,
 }
 
 impl Client {
     fn new(stream: TcpStream) -> Self {
-        let addr = stream.peer_addr().unwrap().to_string();
+        let addr = stream.peer_addr().unwrap();
         Self { stream, addr }
     }
 }
 
-fn handle_client(mut stream: TcpStream, clients: Arc<Mutex<Vec<Client>>>) {
+fn handle_client(mut stream: TcpStream, clients: Arc<Mutex<HashMap<SocketAddr, Client>>>) {
     let mut buffer = [0; 1024];
     loop {
         stream
@@ -43,17 +44,18 @@ fn handle_client(mut stream: TcpStream, clients: Arc<Mutex<Vec<Client>>>) {
         //command to see all connected clients
         if request.trim() == "update" {
             let clients = clients.lock().unwrap();
-            for client in clients.iter() {
-                let response = format!("{}\n", client.addr);
-                stream
-                    .write(response.as_bytes())
-                    .expect("message couldnt be sent");
-            }
+            let response = format!(
+                "{:?}\n",
+                clients.get(&stream.peer_addr().unwrap()).unwrap().addr
+            );
+            stream
+                .write(response.as_bytes())
+                .expect("message couldnt be sent");
             continue;
         }
 
         let remoteaddr: String = stream.peer_addr().unwrap().to_string();
-        let response = format!("you are: {}\n", remoteaddr);
+        let response = format!("HIIII {}", remoteaddr);
         stream
             .write(response.as_bytes())
             .expect("message couldnt be sent");
@@ -61,10 +63,11 @@ fn handle_client(mut stream: TcpStream, clients: Arc<Mutex<Vec<Client>>>) {
 }
 
 fn main() {
-    let clients = Arc::new(Mutex::new(Vec::new()));
+    let clients: Arc<Mutex<HashMap<SocketAddr, Client>>> = Arc::new(Mutex::new(HashMap::new()));
     let addr: &str = "127.0.0.1:6969";
     let listener = TcpListener::bind(addr).expect("couldnt bind to {addr}");
     println!("server listening on {addr}");
+
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
@@ -72,7 +75,10 @@ fn main() {
 
                 {
                     let mut client_list = clients.lock().unwrap();
-                    client_list.push(Client::new(stream.try_clone().unwrap()));
+                    client_list.insert(
+                        stream.peer_addr().unwrap(),
+                        Client::new(stream.try_clone().unwrap()),
+                    );
                 }
                 let clients = clients.clone();
                 std::thread::spawn(move || handle_client(stream, clients));
